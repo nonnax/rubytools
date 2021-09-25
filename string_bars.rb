69700180aff8ea4e8e45857c35d9ac427328847d
nonnax <inonnax@gmail.com>
2021-09-26 02:37:49 +0800

    add string bars. transforms horizontal to vertical ascii bar plots

---
+++ string_bars.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'array_table'
+require 'ansi_color'
+
+class Array
+  def to_hbars(delimeter: ' ')
+    def to_max_width
+      maxwidth = map(&:size).max
+      map!  do |r|
+        l = r.ljust(maxwidth).split(//)
+        sign = r.split(//).last
+        sign == '-' ? l.map(&:red) : l.map(&:cyan)
+      end
+    end
+    to_max_width
+    map(&:reverse).transpose.to_table(delimeter: delimeter)
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  data = []
+
+  data << 'RONALDEVAN'
+  data << 'RON'
+  data << 'RONALD-'
+  data << 'NALD'
+  puts data
+  puts
+  puts data.to_hbars # (30)
+end
