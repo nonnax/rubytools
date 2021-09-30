d0889b4a4ee0c8ebfc1213a933f050262a546a3d
nonnax <ironald@gmail.com>
2021-10-01 04:35:21 +0800

    updates

--- concut.rb
+++ concut.rb
@@ -10,24 +10,23 @@ p inf=Dir["*.*"].fzf.first
 exit unless inf
 sane_name=inf.gsub(/[^\w\d\.]/, '_')
 
-CUTLIST_FILE="cut-#{sane_name}.csv"
-cuttimes_df=ArrayCSV.new(CUTLIST_FILE)
-cuttimes_df.empty? && cuttimes_df<<%w[00:00:00.000 00:00:01.000]
-cuttimes=cuttimes_df.dataframe
+cuts_df=ArrayCSV.new("cut-#{sane_name}.csv")
+cuts_df.empty? && cuts_df<<%w[00:00:00.000 00:00:01.000]
+cuts=cuts_df.map # shared
 
 cmd=[]
-prefix=[]
+istreams=[]
 
 cmd<<"ffmpeg"
 
-cuttimes.each_with_index do |(ss, to), i|
+cuts.each_with_index do |(ss, to), i|
   cmd<<"-ss #{ss} -to #{to} -i '#{inf}'"
-  prefix<<"[%d:v][%d:a]" % [i, i]
+  istreams<<"[%d:v][%d:a]" % [i, i]
 end
-prefilter=prefix.join(' ')
+streams_enum=istreams.join(' ')
 
-cmd<<"-filter_complex '#{prefilter}concat=n=#{cuttimes.size}:v=1:a=1[out]'"
+cmd<<"-filter_complex '#{streams_enum}concat=n=#{cuts.size}:v=1:a=1[out]'"
 cmd<<"-map '[out]'"
 cmd<<"'out-#{sane_name}'"
-p cmd = cmd * ' '
-IO.popen(cmd, &:read)
+p cmd_str = cmd * ' '
+IO.popen(cmd_str, &:read)
--- fzf.rb
+++ fzf.rb
@@ -2,32 +2,45 @@
 
 module Enumerable
   def fzf
-    IO.popen('fzf -m', 'w+') do |io| 
+    IO.popen('fzf -m', 'w+') do |io|
       io
         .puts to_a.join("\n")
       io
         .read
-        .split("\n") 
+        .split("\n")
     end
   end
 
   def fzf_with_index
     IO.popen('fzf -m', 'w+') do |io|
-      io.puts to_a
-                .map
-                .with_index { |e, i| [i, e].join("\t") }
-                .join("\n")
-      # return integer indices 
+      io
+        .puts to_a
+          .map
+          .with_index { |e, i| [i, e].join("\t") }
+          .join("\n")
+      # return integer indices
       io.read
         .split("\n")
-        .map{|e| e.split(/\s/, 2)}
+        .map { |e| e.split(/\s/, 2) }
         .map { |i, e| [i.to_i, e] }
+    end
+  end
+
+  def fzf_map
+    IO.popen('fzf -m', 'w+') do |io|
+      io
+        .puts to_a
+          .map { |k, v| [k, v].join(' : ') }
+      io
+        .read
+        .split("\n")
+        .map { |h| h.split(/\s?:\s?/, 2) }
+        .map { |i, e| [i.to_sym, e] }
     end
   end
 end
 
-
-if __FILE__==$0 then
+if __FILE__ == $PROGRAM_NAME
   p Dir['*.*'].fzf
   p Dir['*.*'].fzf_with_index
   p (1..100).fzf
@@ -35,4 +48,6 @@ if __FILE__==$0 then
   p v = (rand(100..200)..200).fzf
   p 'v = (rand(100..200)..200).to_a.fzf_with_index'
   p v = (rand(100..200)..200).to_a.fzf_with_index
+  h = { name: 'ronald', hobby: 'noodling' }
+  p h.fzf_map
 end
