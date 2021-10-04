d9920ff6702c2bf490b9fdbfc610548112c1ba0e
nonnax <ironald@gmail.com>
2021-10-04 23:14:32 +0800

    updated console.rb

--- console.rb
+++ console.rb
@@ -1,48 +1,62 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 # Id$ nonnax 2021-10-02 23:55:26 +0800
 
 require 'io/console'
 
 module Keyboard
-	extend self
-	
-	def getchar
-	  # nonblocking read of a single keyboard input without echo 
-	  # special chars prefixed are with "[" i.e. "[A" keyboard up arrow
-	  kbinput=[]
-	  case ch=STDIN.getch
-	  when "\e"
-	    2.times{kbinput<<STDIN.getch}
-	    # discard insert/delete/pgup/pgdn trailing char '~'
-	    STDIN.getch if %(2 3 5 6).include?(kbinput.last)
-	    kbinput
-	  else
-	    kbinput<<ch
-	  end.join
-	end
+  module_function
+
+  def getchar
+    # reads keyboard input without echo
+    # special chars are prefixed with "[" i.e. "[A" (up-arrow)
+    kbinput = []
+    case ch = $stdin.getch
+    when "\e"
+      2.times { kbinput << $stdin.getch }
+      # discard insert/delete/pgup/pgdn trailing char '~'
+      $stdin.getch if %(2 3 5 6).include?(kbinput.last)
+      kbinput
+    else
+      kbinput << ch
+    end.join
+  end
 end
 
 module AnsiScreen
-	extend self
-	
-	def printxy(x, y, text)
-		print( "\033[%d;%dH%s" % [x, y, text])
-	end
-	def clear
-		print "\033[2J"
-	end
-	def save
-		print "\033[s"
-	end
-	def restore
-		print "\033[u"
-	end
-	def down(n)
-		print "\033[#{n}A"
-	end
+  module_function
+
+  def printxy(x, y, text)
+    print(format("\033[%d;%dH%s", x, y, text))
+  end
+
+  def clear
+    print "\033[2J"
+  end
+
+  def save
+    print "\033[s"
+  end
+
+  def restore
+    print "\033[u"
+  end
+
+  def down(n)
+    print "\033[#{n}A"
+  end
+
+  def hide_cursor
+    print "\033[?25l"
+  end
+  def show_cursor
+    print "\033[?25h"
+  end
+
 end
 
 module Kernel
-	include Keyboard
-	include AnsiScreen
+  include Keyboard
+  include AnsiScreen
 end
