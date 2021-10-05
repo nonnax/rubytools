ae254f672223c3a65a5a0a65bb18bd2cab5ab772
nonnax <inonnax@gmail.com>
2021-10-05 21:26:51 +0800

    new CLI narrator and basic file cache

---
+++ cache.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env mruby
+# frozen_string_literal: true
+
+# simple tempfile cache
+module Cache
+  module_function
+
+  @ttl = 60 * 30
+
+  def ttl=(val)
+    @ttl = val
+  end
+
+  def ttl
+    @ttl
+  end
+
+  def cache
+    @cache ||= {}
+  end
+
+  def tmp(key)
+    "/tmp/#{key}"
+  end
+
+  def valid?(key)
+    cached_file = tmp(key)
+    File.exist?(cached_file) && (Time.now - File.open(cached_file).mtime <= @ttl)
+  end
+
+  def get(key)
+    return nil unless valid?(key)
+
+    File.read(tmp(key))
+  end
+  alias [] get
+
+  def set(key, content)
+    cache[key] = tmp(key)
+    File.open(tmp(key), 'w').puts(content)
+    content
+  end
+  alias []= set
+
+  def cached(key = nil, **h)
+    self.ttl = h[:ttl] if h[:ttl]
+    if result = get(key)
+      result
+    else
+      result = yield
+      set(key, result)
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  loop do
+    res = Cache.cached('testkey', ttl: 10) do
+      IO.popen('coingecko').read
+    end
+    puts Time.now
+    puts res
+    sleep 1
+  end
+end
---
+++ narrate.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+# narrate.rb 
+# 	narrates a file or a piped stream 
+# 	uses espeak or festival
+# 
+# Id$ nonnax 2021-10-05 14:44:14 +0800
+require 'console'
+
+maxx, maxy = $stdout.winsize
+
+def ARGS
+	# intercepts input params either as arguments or piped str
+	#
+  content =
+    if !$stdin.tty? && !$stdin.closed?
+      $stdin.read
+    elsif !ARGV.empty?
+      ARGV
+    end
+  yield content
+end
+
+content, start = ARGS() do |arg|
+  case arg
+  when Array
+    f, start = arg
+    [File.read(f), start.to_i]
+  when String
+    [arg, 0]
+  else
+    exit
+  end
+end
+
+module SentenceScanner
+  RE_SENTENCE = /[^.?!]+[^)](?:[.?!)])/.freeze
+
+  def sentences
+    gsub!(/\n/, ' ')
+    keys = %w[“ ” ‘ ’]
+    vals = %w[" " ' ']
+    to_tr = keys.zip(vals).to_h
+    to_tr.each { |k, v| tr!(k, v) }
+
+    scan(RE_SENTENCE).map # {|e| yield e}
+  end
+end
+
+String.include(SentenceScanner)
+
+content.sentences.to_a[start..-1].each_slice(3) do |s|
+  s.each do |e|
+    puts e.strip.center(maxy)
+  end
+
+  # IO.popen("festival --tts", 'w'){|io| io.puts s.join}
+  IO.popen('espeak --stdin &>/dev/null', 'w') { |io| io.puts s.join }
+end
