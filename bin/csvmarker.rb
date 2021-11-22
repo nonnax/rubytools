c8fdaabd1916ac8222722f9fec9611c2141faaf7
nonnax <ironald@gmail.com>
2021-11-22 23:39:32 +0800

    new csv marker tool & fix

---
+++ bin/csvmarker.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# require 'text-table'
+require 'rubytools/numeric_ext'
+require 'rubytools/arraycsv'
+require 'rubytools/time_and_date_ext'
+require 'rubytools/array_table'
+require 'rubytools/ansi_color'
+
+files = ARGV
+
+def view_as_table(f)
+  data = ArrayCSV.new(f)
+
+  human_df = data.dataframe.map do |r|
+    r.map do |e|
+      if e.to_s.is_number? 
+        e.to_f.commify
+      elsif (e.to_s.is_date? and not e.to_s.is_number?)
+        e.to_s.to_date.to_days
+      elsif e.nil?
+        e="-"
+      else
+        e
+      end
+    end
+  end
+
+  i=true
+  table = human_df.to_table( rjust: (0..human_df.first.size), delimeter: ' '*3 ) do |r|
+    row = i ? r.join(' ') : r.join(' ').light_magenta
+    row = r.join(' ').yellow if (r.last.strip.is_number? && r.last.strip.to_f > 0 ) 
+    i = !i
+    row 
+  end
+
+  puts table
+end
+
+files.each do |f|
+  view_as_table(f)
+end
--- rubytools/hash_ext.rb
+++ rubytools/hash_ext.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2021-11-16 10:11:27 +0800
+require 'cgi'
+require 'uri'
+
 class Hash
   def to_query_string
     inject([]) do |a, (k, v)|
@@ -13,8 +16,6 @@ class Hash
   end
 end
 
-require 'cgi'
-require 'uri'
 class String
   def query_string_to_h
     CGI.parse(URI.parse(self).query).transform_keys(&:to_sym)
