0ad08b822d739058d16409bfa204d366342d6529
nonnax <ironald@gmail.com>
2021-12-16 18:14:14 +0800

    update: editor.rb

--- rubytools/editor.rb
+++ rubytools/editor.rb
@@ -1,11 +1,25 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2021-10-02 17:23:14 +0800
+require 'tempfile'
 class IO
-  def self.editor(text="", editor: ENV['EDITOR'])
-    IO.popen(editor, 'w+') do |io|
-      io.puts text
-      io.close_write
-      io.read
+  class << self
+    def buffer(text, editor)    
+      IO.popen(editor, 'w+') do |io|
+        io.puts text
+        io.close_write
+        io.read
+      end  
+    end
+    def tempfile(text, editor)
+      Tempfile.create('internal') do |f|
+        f.puts text
+        f.rewind
+        cmd=[editor, f.path].join(' ')
+        IO.popen(cmd, &:read)
+      end
+    end
+    def editor(text="", editor: ENV['EDITOR'], with: :buffer)
+      send with, text, editor
     end
   end
 end
