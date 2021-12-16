3df841ef9ba4133110e9a4a9b1000a6d40cc4539
nonnax <ironald@gmail.com>
2021-12-16 19:35:50 +0800

    update: catcsv.rb

--- bin/catcsv.rb
+++ bin/catcsv.rb
@@ -3,10 +3,10 @@
 
 # Id$ nonnax 2021-09-27 01:40:36 +0800
 require 'rubytools'
-require 'rubytools/arraycsv'
-require 'rubytools/array_table'
-require 'rubytools/numeric_ext'
-require 'rubytools/fzf'
+require 'arraycsv'
+require 'array_table'
+require 'numeric_ext'
+require 'fzf'
 
 
 csvstr = $stdin.read
