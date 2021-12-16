28cbcc3f081bd956fdebf0f5aa970c9f343c945a
nonnax <ironald@gmail.com>
2021-12-16 22:07:57 +0800

    update: coingecko.rb

--- bin/coingecko.rb
+++ bin/coingecko.rb
@@ -3,9 +3,14 @@ require "json"
 require "uri"
 require "net/http"
 require "rubytools/cache"
+require "rubytools/hash_ext"
 require 'excon'
 
+
 module Coingecko
+  # url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=#{coins.join(',')}
+  # &order=market_cap_desc&per_page=100
+  # &page=1&sparkline=false&price_change_percentage=24h,7d,14d,30d,90d"
   VERSION = "0.1.0"
   # not found
   extend self 
@@ -23,9 +28,19 @@ module Coingecko
     "compound-coin",
     "basic-attention-token"]
     list=[]
-
-    # url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=#{coins.join(',')}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h,7d,14d,30d,90d"
-    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=#{coins.join(',')}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h,7d,14d,30d,90d"
+    
+    qstr={
+      vs_currency:'php',
+      ids: coins.join(','),
+      order: 'market_cap_desc',
+      per_page: 100,
+      page:1,
+      sparkline: false,
+      price_change_percentage: %w[24h 7d 14d 30d 90d]
+    }
+    
+    url = ["https://api.coingecko.com/api/v3/coins/markets", qstr.to_query_string].join('?')
+    
     res=Excon.get(url)
     data=JSON.parse(res.body, symbolize_names: true)
     keys=%i[id current_price high_24h low_24h price_change_percentage_24h price_change_percentage_30d_in_currency]
