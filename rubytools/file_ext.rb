b9c2f76229b5b5dc4799ca173a501b2f3c59d955
nonnax <ironald@gmail.com>
2021-12-16 13:37:18 +0800

    update: file_ext.rb

--- rubytools/file_ext.rb
+++ rubytools/file_ext.rb
@@ -13,5 +13,30 @@ module SafeFileName
   end
 end
 
+module NumberedFile
+  def filename_succ
+    basename = File.basename(self, '.*')
+    ext = File.extname(self)
+    out = nil
+    bn=basename.dup
+    loop do
+      bn = bn.match(/\d+$/) ? bn.succ : "#{bn}_001"
+      n=bn.match(/\d+$/).to_s.to_i.to_s
+      bn.gsub!(/\d+$/, n.rjust(3, '0'))      
+      out = "#{bn}#{ext}"
+      break unless File.exist?(out)
+    end
+    out
+  end
+end
+
+module SafeFileName
+  def to_safename
+    gsub(/[^\w\.]/, '_')
+  end
+end
+
 String.include(SafeFileName)
+String.include(SafeFileName)
+String.include(NumberedFile)
 
