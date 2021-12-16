a44b55fd77af9f60e90ecdea7f34443511efb8ef
nonnax <ironald@gmail.com>
2021-12-16 14:09:30 +0800

    update: xxhsum.rb

--- rubytools/xxhsum.rb
+++ rubytools/xxhsum.rb
@@ -1,19 +1,15 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2021-11-30 00:16:10 +0800
 module XXHSum
-  def xxhsum()
-    IO.popen("xxhsum", "w+") do |io| 
+  def xxhsum(h: 64)
+    IO.popen("xxhsum -H#{h}", "w+") do |io| 
       io.puts self 
       io.close_write 
       io.read
     end.split(/\s+/).first
   end
   def xxh32sum()
-    IO.popen("xxh32sum", "w+") do |io| 
-      io.puts self
-      io.close_write 
-      io.read
-    end.split(/\s+/).first
+      xxhsum(h: 32)
   end
 
 end
