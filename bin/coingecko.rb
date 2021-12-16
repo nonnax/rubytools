0f7fdd4fe90edf40067e3ab5e13faba74434825a
nonnax <ironald@gmail.com>
2021-12-16 21:59:38 +0800

    update: coingecko.rb

--- bin/coingecko.rb
+++ bin/coingecko.rb
@@ -11,10 +11,10 @@ module Coingecko
   extend self 
   def get
     coins=["bitcoin", 
-    "ethereum", 
-    "ripple", 
-    "bitcoin-cash", 
-    "litecoin", 
+    "ethereum",
+    "ripple",
+    "bitcoin-cash",
+    "litecoin",
     "the-graph",
     "aave",
     "uniswap",
@@ -25,15 +25,16 @@ module Coingecko
     ]
     list=[]
 
+    # url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=#{coins.join(',')}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h,7d,14d,30d,90d"
     url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&ids=#{coins.join(',')}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h,7d,14d,30d,90d"
     res=Excon.get(url)
     data=JSON.parse(res.body, symbolize_names: true)
-    keys=%i[id current_price high_24h low_24h price_change_percentage_24h price_change_percentage_30d]
+    keys=%i[id current_price high_24h low_24h price_change_percentage_24h price_change_percentage_30d_in_currency]
 
     data.each do |coin|
-      list<< coin.values_at(*keys).compact.join(",")
+      list<< coin.values_at(*keys).join(',')
     end 
-    list
+    list.prepend(keys.map(&:to_s).map{|e| e.gsub(/change_percentage/, 'delta').gsub(/in_currency/,'php')}.join(','))
   end
   
 end
@@ -42,4 +43,5 @@ res=Cache.cached('coingecko', ttl: 30) do
   Coingecko.get
 end
 
+# p Coingecko.get
 puts res
