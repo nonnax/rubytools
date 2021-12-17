c1bd6f00e984a0c0fd4b437352f7dff3d5422fbd
nonnax <ironald@gmail.com>
2021-12-17 12:42:34 +0800

    update: narrate.rb

--- bin/narrate.rb
+++ bin/narrate.rb
@@ -11,8 +11,8 @@ require 'rubytools/string_ext'
 maxx, maxy = IO::Screen.winsize
 
 def ARGS
-	# intercepts input params either as arguments or piped str
-	#
+  # intercepts input params either as arguments or piped str
+  #
   content =
     if !$stdin.tty? && !$stdin.closed?
       $stdin.read
@@ -39,14 +39,14 @@ CHUNK_SIZE=1
 para_size=content.paragraphs.each_slice(CHUNK_SIZE).to_a.size
 
 content.paragraphs.each_slice(CHUNK_SIZE).each_with_index do |cont, i|
-	puts paragraph_chunk=cont.join("\n\n")
-	puts "%d/%d. (%d)." % [i+1, para_size, paragraph_chunk.size]
-	puts
-	p mp3=Dir["*.mp3"].grep( Regexp.new("-%02d" % [i+1]))
-	puts "play '#{mp3.first}' &>/dev/null"
-	IO.popen("mpv --loop-playlist=no --loop-file=no '#{mp3.first}' &>/dev/null"){|io| io.read}
-	IO.popen('espeak --stdin &>/dev/null', 'w') { |io| io.puts paragraph_chunk }
-	sleep 3
-	puts
+  puts paragraph_chunk=cont.join("\n\n")
+  puts "%d/%d. (%d)." % [i+1, para_size, paragraph_chunk.size]
+  puts
+  p mp3=Dir["*.mp3"].grep( Regexp.new("-%02d" % [i+1]))
+  puts "play '#{mp3.first}' &>/dev/null"
+  IO.popen("mpv --loop-playlist=no --loop-file=no '#{mp3.first}' &>/dev/null"){|io| io.read}
+  IO.popen('espeak --stdin &>/dev/null', 'w') { |io| io.puts paragraph_chunk }
+  sleep 3
+  puts
 end
 
