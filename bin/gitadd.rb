2e78cc77f08568f120425eed126f1d375c186936
nonnax <ironald@gmail.com>
2022-02-10 15:11:14 +0800

    update: gitadd.rb

--- bin/gitadd.rb
+++ bin/gitadd.rb
@@ -2,7 +2,10 @@
 # Id$ nonnax 2021-10-19 14:15:56 +0800
 require 'rubytools/fzf'
 
-files=ARGF.readlines(chomp: true).sort.reverse.fzf(cmd: %{fzf -m --preview='git status'})
+cmd="git status 2>1 | ruby -ne 'puts $_.chomp.split(/:\s+/).last if $_.match(/fied\:/)'"
+modified=IO.popen(cmd, &:read)
+
+files=modified.lines(chomp: true).sort.reverse.fzf(cmd: %{fzf -m --preview='git status'})
 
 cmd="git add "<<files.join(' ')
 
