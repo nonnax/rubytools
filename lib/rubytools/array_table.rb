3f5026c8692cd57662ab606fd71037ed3d29ee6c
nonnax <ironald@gmail.com>
2022-02-14 21:43:32 +0800

    update: array_table.rb

--- lib/rubytools/array_table.rb
+++ lib/rubytools/array_table.rb
@@ -9,11 +9,11 @@
 #   df << Array.new(5) { rand(100_000).to_f }
 # end
 # 
-# renders a dataframe into table (String)
+# renders a dataframe into string formatted table
 #
 # puts df.to_table(delimeter: ' / ',	rjust: [0, 1, 2], ljust: (3..5))
 #
-# a block yields an array of rendered elements (String) 
+# a block yields arrays of rendered string elements 
 #
 # df.to_table(delimeter: ' / ',	rjust: [0, 1, 2], ljust: (3..5)) do |row|
 #   puts row.reverse
@@ -34,29 +34,41 @@ module ArrayTable
 
   def to_table(df = self, **h, &block)
     # renders a dataframe into table
+    # align_method keys = :center, :ljust, :rjust
+    # puts df.to_table(delimeter: ' / ',	rjust: [0, 1, 2], ljust: (3..5))
+    # ...
+    # or
+    # ...
+    # df.to_table(delimeter: '/',	rjust: [0, 1, 2], ljust: (3..5)) do |row|
+    #   puts row.reverse
+    # end
+
     align_keys = %i[ljust rjust center]
     delimeter = h[:delimeter] || ' | '
     unless (align_keys & h.keys).empty?
       align_method, selected_columns = h.select { |k, _| align_keys.include?(k) }.to_a.first
     end
+
     # column widths
     column_width = {}
     df.first.size.times { |i| column_width[i] = 0 }
 
-    # detect max width and init column widths
+    # detect max width to become fixed column widths
     df.dup.transpose.each_with_index do |e, i|
       column_width[i] = e.map(&:to_s).map(&:size).max
     end
+
     # all table items become strings
     # column widths are adjusted to max widths
     prep = df.transpose.map.with_index do |r, i|
       r.map(&:to_s).map { |e| e.to_s.rjust(column_width[i]) }
     end
+    
     # adjust specific cols selected
     # selected_columns=(0..df.first.size)
     # align_method = :center
     # align_method = :rjust
-    #
+
     if align_method
       prep = prep.map.with_index do |r, i|
         r.map! { |e| selected_columns.include?(i) ? e.strip.send(align_method, column_width[i]) : e } 
@@ -70,33 +82,36 @@ module ArrayTable
       else
         r.join(delimeter)
       end
-    end #.join("\n")
+    end
+  # rescue => e
+    # p e
   end
 
   #helper methods
 
-  def each_slice_safe(n, padding: nil)
+  def each_slice_safe(n=1, padding: nil)
     # returns equally sliced rows
     # padded with <padding>
-    # returns an Enumerator
-    arr=each_slice(n).to_a
-    arr.pad_rows(padding:)
+    enum=each_slice(n)
+    enum
+     .pad_rows(padding:)
+     .to_enum
   end
 
   def pad_rows(padding:nil)
-    # returns equally resized rows
-    # returns an Enumerator
+    # resizes short rows
+    # returns an Array dataframe
     max_size=map(&:size).max
     dup.map do |d|
       d + [padding] * (max_size - d.size)
-    end.map
+    end
   end
   alias auto_resize_rows pad_rows
-
 end
 
 Enumerable.include(ArrayTable)
-# Enumerable.include(ArrayTable)
+
+# convenience method for terminal piped strings
 
 require 'csv'
 
@@ -105,7 +120,6 @@ class String
     data=CSV.parse(self, converters: :numeric, col_sep:)
     data
       .pad_rows
-      .to_a    
       .to_table(delimeter:)
   end
 end
