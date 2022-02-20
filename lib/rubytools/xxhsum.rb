523d17b8c8d355301d60486487ff841d26c4a3a4
nonnax <ironald@gmail.com>
2022-02-20 18:46:55 +0800

    update: xxhsum.rb

--- lib/rubytools/xxhsum.rb
+++ lib/rubytools/xxhsum.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 # Id$ nonnax 2021-11-30 00:16:10 +0800
+require 'ruby-xxhash'
+
 module XXHSum
   def xxhsum(h: 64)
     # IO.popen("xxhsum -H#{h}", "w+") do |io|
@@ -15,6 +17,12 @@ module XXHSum
   def xxh32sum
     xxhsum(h: 32)
   end
+
+  def xxhash(seed: 0, bit64: false)
+    message = bit64 ? :xxh64 : :xxh32
+    XXhash.send(message, self, seed)
+  end
+  
 end
 
 String.include(XXHSum)
