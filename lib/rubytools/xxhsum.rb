2d191d6eedb4cb8402df5b9e5f0a488978aa84fb
nonnax <ironald@gmail.com>
2022-02-21 16:47:36 +0800

    update: xxhsum.rb

--- lib/rubytools/xxhsum.rb
+++ lib/rubytools/xxhsum.rb
@@ -6,7 +6,6 @@ require 'ruby-xxhash'
 
 module XXHSum
   def xxhsum(h: 64)
-    # IO.popen("xxhsum -H#{h}", "w+") do |io|
     IO.popen('xxhsum', 'w+') do |io|
       io.puts self
       io.close_write
@@ -14,11 +13,16 @@ module XXHSum
     end.split(/\s+/).first
   end
 
-  def xxh32sum
-    xxhsum(h: 32)
+  def xxh32sum(seed:)
+    xxhash(seed:)
+  end
+
+  def xxh64sum(seed:)
+    xxhash(seed:, bit64: true)
   end
 
   def xxhash(seed: 0, bit64: false)
+    # fast version
     message = bit64 ? :xxh64 : :xxh32
     XXhash.send(message, self, seed).to_s(36)
   end
