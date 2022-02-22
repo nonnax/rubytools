54bceab50b54e6bc5827fdbe677ebbc45b59c8e5
nonnax <ironald@gmail.com>
2022-02-22 23:19:08 +0800

    update: ascii_bars.rb

--- lib/rubytools/ascii_bars.rb
+++ lib/rubytools/ascii_bars.rb
@@ -5,19 +5,22 @@ require 'rubytools/array_table'
 require 'rubytools/ansi_color'
 
 class Array
-  def to_hbars(delimeter: ' ')
-    def to_max_width
-      maxwidth = map(&:size).max
+  def to_hbars(delimiter: ' . ', &block)
+    def expand_rows(&block)
+      max_width = map(&:size).max
       map!  do |r|
-        l = r.ljust(maxwidth).split(//)
         sign = r.split(//).last
-        sign == '-' ? l.map(&:red) : l.map(&:cyan)
+        marked=sign.match('-')
+        r=r.chomp('-') if marked
+        l = r.ljust(max_width).split(//) # split each element 
+        l = block[l, marked]
       end
     end
-    to_max_width
-    map(&:reverse)
-      .transpose
-      .to_table(delimeter: delimeter)
+
+    dup.expand_rows{|bar, marked| marked ? bar.map(&:red) : bar.map(&:cyan) }
+    .map(&:reverse)
+    .transpose
+    .to_table(delimiter:)
   end
 end
 
@@ -26,9 +29,15 @@ if __FILE__ == $PROGRAM_NAME
 
   data << 'RONALDEVAN'
   data << 'RON'
-  data << 'RONALD-'
-  data << 'NALD'
+  data << 'RONALD-'  # a trailing dash (-) renders a red bar
+  data << 'NALD-DUDE' # non-trailing dashes (-) are ignored
+  puts '# data'
   puts data
-  puts
-  puts data.to_hbars # (30)
+
+  puts "# 2.times{ |i| puts data.to_hbars(delimiter: ' '*i)  }"
+  2.times{ |i| puts data.to_hbars(delimiter: ' '*i)  }
+
+  puts '# puts data.to_hbars'
+  puts data.to_hbars
+
 end
