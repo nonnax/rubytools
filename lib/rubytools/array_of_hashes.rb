ffec4e4fc54cfeb1fc5f8bd16d2263d4c4ded41d
nonnax <ironald@gmail.com>
2022-02-26 14:26:15 +0800

    update: array_of_hashes.rb

--- lib/rubytools/array_of_hashes.rb
+++ lib/rubytools/array_of_hashes.rb
@@ -26,18 +26,73 @@ module ArrayOfHashes
 
     select{ |d| eval parsed_query }
   end
+
+
+  def grep_hash(**criteria, &block)
+    # grep_hash(..., (:all?|:any?): true)
+    # grep_hash({...}){|join| join.all?}
+    # (any?: default)
+    # dbase = [
+    #   {username: 'nald', location: 'here', age: 118},
+    #   {username: 'nald', location: 'home',  age: 21},
+    #   {username: 'nonnax', location: 'home',  age: 18}
+    # ]
+    # 
+    # p dbase.grep_hash(username: '\bna', age: 18, all?: true)
+    # # all?:|any?: only key presence significant. any value is valid
+    # p dbase.grep_hash(username: '\bna', age: 18, all?: 'yes')
+    # p dbase.grep_hash(username: '\bna', age: 18, all?: 1)
+    # p dbase.grep_hash(username: '\bna', age: 18, all?: nil)
+    # # using a block for joins
+    # p dbase.grep_hash(username: '\bna', age: 18){|join| join.any?}
+    # p dbase.grep_hash(username: '\bna', age: 18){|e| e.all?}
+    # p dbase.grep_hash(username: '\bna', age: 18){ false } # -> []
+
+    join=criteria.key?(:all?) && :all?
+    join ||= :any?
+    # all?:|any?: only key presence significant. any value is valid
+    select do |hsh|
+      crit=
+        criteria
+          .except(:any?, :all?)
+          .map{|k, v|
+            # criterias are regexp(s) 
+            re = Regexp.new(v.to_s)
+            hsh[k].to_s.match(re)
+        }
+      if block
+        block[crit] 
+      else
+        crit.send(join)
+      end
+    end
+    rescue => e
+      puts [
+        e, 
+        "#{__method__}( #{criteria.inspect} )", 
+        e.backtrace
+      ]
+  end
 end
 
 Array.include(ArrayOfHashes)
-
 # 
 if __FILE__==$0 then
   dbase = [
-    {username: 'nald', location: 'here', age: 11}, 
+    {username: 'nald', location: 'here', age: 118}, 
     {username: 'nald', location: 'home',  age: 21},
     {username: 'nonnax', location: 'home',  age: 18}
   ]
 
   p 'dbase.query(username: /no/)'
   p dbase.query(username: /no/)
+  p 'dbase.grep_hash(username: '\bna', age: 18, all?: true)'
+  p dbase.grep_hash(username: '\bna', age: 18, all?: true)
+  # all?:|any?: only key presence significant. any value is valid
+  p dbase.grep_hash(username: '\bna', age: 18, all?: 'yes')
+  p dbase.grep_hash(username: '\bna', age: 18, all?: 1)
+  p dbase.grep_hash(username: '\bna', age: 18, all?: nil)
+  p dbase.grep_hash(username: '\bna', age: 18){|join| join.any?}
+  p dbase.grep_hash(username: '\bna', age: 18){|join| join.all?}
+  p dbase.grep_hash(username: '\bna', age: 18){ false }
 end
