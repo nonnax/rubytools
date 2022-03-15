757feee8846c20afe5e65773a2820687a27fc7ec
nonnax <iroanld@gmail.com>
2022-03-15 15:01:17 +0800

    update: pipe_argv.rb

--- lib/rubytools/pipe_argv.rb
+++ lib/rubytools/pipe_argv.rb
@@ -4,8 +4,7 @@
 # Id$ nonnax 2021-12-12 22:48:44 +0800
 
 def PIPE_ARGV
-  key = ARGV.first
+  key = ARGV.shift
   return key if key
-
-  key = $stdin.gets unless $stdin.tty? && $stdin.closed?
+  key = ARGF.gets
 end
