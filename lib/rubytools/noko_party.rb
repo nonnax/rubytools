f3feaeb672f473ba70efb44c991df4eb47823828
nonnax <ironald@gmail.com>
2022-05-18 14:35:20 +0800

    refactor

--- lib/rubytools/noko_party.rb
+++ lib/rubytools/noko_party.rb
@@ -8,9 +8,10 @@ require 'httparty'
 class NokoParty
   BROWSER = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1'
   def self.get(url)
-    html = HTTParty
-           .get(url, browser: BROWSER)
-           .body
-    Nokogiri::HTML(html)
+    HTTParty
+     .get(url, browser: BROWSER)
+     .body
+     .force_encoding('UTF-8')
+     .then{ |html| Nokogiri::HTML(html) }
   end
 end
