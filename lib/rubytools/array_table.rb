f613dffe3e127503f7e3899d6a14eab4ba36b882
nonnax <ironald@gmail.com>
2022-06-03 13:30:11 +0800

    update

--- lib/rubytools/array_table.rb
+++ lib/rubytools/array_table.rb
@@ -7,18 +7,25 @@ class Table < SimpleDelegator
   def template    
     s=<<~___
       <% self.to_a.each do |e| %>
-        <%= e.map{|f| f.to_s.rjust(padding)} %>
+        <%= e.map{|f| f.to_s.send(justify, padding)}.join(delimiter) %>
       <% end %>
     ___
   end
 
   def padding()=@padding||=3
+  def delimiter=(d) @delimiter=d  end
+  def delimiter(); @delimiter ||=' ' end
   def set_padding=(n); @padding=n end
   def auto_padding
     @padding=self.to_a.flatten.map{|e| e.to_s.size }.max
   end
 
-  def to_s
+  def justify
+    @ljust ? :ljust : :rjust
+  end
+
+  def to_s(ljust:true)
+    @ljust=ljust
     auto_padding
     ERB.new(template, trim_mode: '<>').result(binding)
   end
