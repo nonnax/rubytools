4ba94227a2da85c3b6c09ccc969d9926f3fe0c46
nonnax <ironald@gmail.com>
2022-11-17 23:31:58 +0800

    add file_importer

---
+++ lib/file/file_importer.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# Id$ nonnax 2022-08-24 15:19:03
+class FileImporter
+  # embed `afile` with @import '<afile>' directive
+  RE_IMPORT = /
+    @import\s*(\W)([\w.\/]+)\1
+  /xm
+  # any pair of non-word delimeters must match
+  # @import 'file'
+  # @import !file!
+  # @import %file%
+  def initialize(dir:'./')
+    @dir=dir
+  end
+  # gsub_imports/puts is recursive.
+  # if @import `afile` has another @import 'bfile' in it, it calls same method on itself
+  def gsub_imports(_text)
+    _text.gsub(RE_IMPORT){ File.read(File.join(@dir, Regexp.last_match[2])) }
+         .then{|text| text.match?(RE_IMPORT) ? gsub_imports(text) : text }
+  end
+
+  def self.parse(text, dir: './')
+    new(dir: ).parse(text)
+  end
+
+  alias_method :puts, :gsub_imports
+  alias_method :parse, :gsub_imports
+end
