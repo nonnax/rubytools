6785bfedbea2bdc76db5f9f7f0122463c7fb0cb4
nonnax <ironald@gmail.com>
2022-11-26 16:25:24 +0800

    add diff method

--- lib/df/df.rb
+++ lib/df/df.rb
@@ -2,17 +2,17 @@
 # frozen_string_literal: true
 
 # Id$ nonnax 2022-11-17 17:49:10
-$LOAD_PATH<<'.'
+$LOAD_PATH << '.'
 
 require 'forwardable'
 
 class DF
   # dataframe
   # @rows -> [
-    # row0 -> [*items],
-    # row1 -> [*items],
-    # row2 -> [*items],
-    # row3 -> [*items]
+  # row0 -> [*items],
+  # row1 -> [*items],
+  # row2 -> [*items],
+  # row3 -> [*items]
   # ]
   # @rows.map(&:size).uniq==1 or raise ExceptionDifferentSizes
   # def <<(arow)
@@ -21,17 +21,17 @@ class DF
   # end
 
   extend Forwardable
-  def_delegators :@rows, :each, :map, :values_at
+  def_delegators :@rows, :each, :map, :values_at, :first, :last
   # first row determines the size
   attr_accessor :rows
 
   alias to_a rows
 
   def initialize(rowsize = 0, truncate: true, &block)
-    @rows = [] #Array.new(rowsize) { nil }
+    @rows = [] # Array.new(rowsize) { nil }
     @row_size = rowsize
     @truncate = truncate
-    update(block.call()) if block
+    update(block.call)
   end
 
   def check_size(row)
@@ -49,13 +49,13 @@ class DF
   def update(df)
     @rows.clear
     df.each do |row|
-      @rows<<check_size(row) if @truncate
+      @rows << check_size(row) if @truncate
     end
   end
 
   def <<(row)
     row = check_size(row) if @truncate
-    raise "Different size: row array size should be #{@row_size}" unless [row.size, @row_size].uniq.size==1
+    raise "Different size: row array size should be #{@row_size}" unless [row.size, @row_size].uniq.size == 1
 
     @rows << row
   end
@@ -68,17 +68,26 @@ class DF
     df
   end
 
+  def diff(another, &block)
+    b = another.dup
+    to_a.map.with_index do |r, i|
+      r.map.with_index do |v, j|
+        block.call(v, b[i][j]) ? "[#{v}]" : v
+      end
+    end
+  end
+
   def to_s(**params, &block)
-    col_widths=@rows.dup.transpose.map{|r| r.map(&:to_s).map(&:size).max}
+    col_widths = @rows.dup.transpose.map { |r| r.map(&:to_s).map(&:size).max }
     @rows.dup.map do |r|
-      just=params[:ljust] ? :ljust : :rjust
+      just = params[:ljust] ? :ljust : :rjust
       # apply formatting to each element
       r.map(&:to_s)
-       .map.with_index{|s, i| s.send(just, col_widths[i])}
-       .join(params.fetch(:delimiter, '  '))
+       .map.with_index { |s, i| s.send(just, col_widths[i]) }
+       .join(params.fetch(:separator, '  '))
     end
-    .join("\n")
-    .tap { |s| block.call(s) if block }
+         .join("\n")
+         .tap { |s| block&.call(s) }
   end
 end
 
