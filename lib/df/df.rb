9020a85780f49ce6ca2c971f7da50d25f03eaa82
nonnax <ironald@gmail.com>
2022-11-29 20:36:38 +0800

    fixed missing `symbols` params

--- lib/df/df.rb
+++ lib/df/df.rb
@@ -46,12 +46,13 @@ class DF
 
   def update(df, cols: nil)
     return self unless df
+
     # widest df column determines the column size unless <cols> is defined
 
     @rows.clear
     @cols = cols if cols
-    @cols = df.map(&:size).max unless @cols
-    df.each {|row| self << row }
+    @cols ||= df.map(&:size).max
+    df.each { |row| self << row }
     self
   end
 
@@ -66,60 +67,68 @@ class DF
     DF.new { @rows.transpose }
   end
 
-  def diff(another, **params, &block)
-    prefix=params.fetch(:prefix, '\\')
-    b = another.to_a
-    DF.new {
-      self
-      .to_a
-      .map
-      .with_index do |r, i|
-        r.map.with_index do |v, j|
-          bv = b[i][j]
-          cond = block&.call(v, bv) || v != bv
-          cond ? "#{prefix}#{v}" : v
-        end
-      end
-    }
-  end
-
   def to_s(**params, &block)
-    view_rows=@rows.dup
+    view_rows = @rows.dup
+
+    fixed_width = params.fetch(:width, nil)
+    separator = params.fetch(:separator, '  ')
+    just = params[:ljust] ? :ljust : :rjust
+
+    view_rows.prepend(view_rows.first.size.times.map.to_a) if params.fetch(:header, nil)
 
     col_widths = @rows.dup.transpose.map { |r| r.map(&:to_s).map(&:size).max }
-    fixed_width = params.fetch(:width, nil)
-    header = params.fetch(:header, nil)
-    view_rows.prepend(view_rows.first.size.times.map.to_a) if header
     col_widths.map! { fixed_width } if fixed_width
 
+
     view_rows
-    .map do |r|
-      just = params[:ljust] ? :ljust : :rjust
+      .map do |r|
       # apply formatting to each element
       r.map(&:to_s)
        .map.with_index { |s, i| s.send(just, col_widths[i]) }
-       .join(params.fetch(:separator, '  '))
-     end
-    .join("\n")
-    .tap { |s| block&.call(s) }
+       .join(separator)
+    end
+      .join("\n")
+      .tap { |s| block&.call(s) }
   end
 end
 
 class DF
- def rc(row, col, index_by: 1)
-   # row, col = [row, col].map{|e| e-1} if index_by.positive?
-   row, col = [row.clamp(0, rows.size-1), col.clamp(0, rows.first.size-1)]
-   rows[row][col].dup
- end
- def [](*cols, &block)
-   # select columns and apply a function on them
-   # return a new DF result
-   DF.new do
-    rows.map.with_index do |r, i|
-      [r, block.call(*cols.map{|col| rc(i, col)})].flatten
+  def diff(another, **params)
+    symbols = [-1, 0, 1].zip('<=>'.split(//)).to_h
+    symbols.merge!([-1, 0, 1].zip(params[:symbols]).to_h) if params[:symbols]
+
+    width = params.fetch(:width, 2)
+
+    str_just = lambda { |x, dir|
+      [x, symbols[dir]].join(' ')
+    }
+    b = another.to_a
+    v = to_a.map.with_index do |r, i|
+      r.map.with_index do |v, j|
+        bv = b[i][j]
+        cond = v <=> bv
+        str_just[v, cond]
+      end
     end
-   end
- end
+    DF.new { v }
+  end
+
+  def rc(row, col, index_by: 1)
+    # row, col = [row, col].map{|e| e-1} if index_by.positive?
+    row = row.clamp(0, rows.size - 1)
+    col = col.clamp(0, rows.first.size - 1)
+    rows[row][col].dup
+  end
+
+  def [](*cols, &block)
+    # select columns and apply a function on them
+    # return a new DF result
+    DF.new do
+      rows.map.with_index do |r, i|
+        [r, block.call(*cols.map { |col| rc(i, col) })].flatten
+      end
+    end
+  end
 end
 
 # converts a compatible array into a DF object
@@ -131,7 +140,7 @@ class ArrayDF < SimpleDelegator
   def rotate_left
     ArrayDF.new(self).to_df # auto-fill
            .then { |df| ArrayDF.new(df.to_a.map(&:reverse)).to_df }
-           .then { |df| df.transpose}
+           .then(&:transpose)
   end
 end
 
