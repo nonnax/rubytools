15c3a783879bb861d3db356d7c7d92d9a2552249
nonnax <ironald@gmail.com>
2022-11-30 16:14:22 +0800

    renamed method to threads

--- lib/thread/n_threads_ext.rb
+++ lib/thread/n_threads_ext.rb
@@ -4,25 +4,27 @@
 # using NThreadsExt
 # usage:
 # q=1500.thread_queues
-# 16.thread_pools{
+# 16.threads{
   # xprocess(q.pop()) until q.empty?
 # }
 # or single method call
-# 16.thread_pools(queue: 1500){|q|
+# 16.threads(queue: 1500){|q|
   # xprocess(q.pop()) until q.empty?
 # }
 
 module NThreadsExt
   refine Integer do
-    def thread_pools(queue:1, &block)
-      q=queue.thread_queues
-      qblock=->{
-        block.call(q)
-      }
-      Array.new(self){Thread.new(&qblock)}.map(&:join)
+    def threads(queue:1, &block)
+      q = queue.thread_queues
+      qblock = ->{ block.call(q) }
+      Array
+      .new(self){ Thread.new(&qblock) }
+      .map(&:join)
     end
     def thread_queues
-      Queue.new.tap{|q|
+      Queue
+      .new
+      .tap{|q|
         self.times{|i| q.push i }
       }
     end
