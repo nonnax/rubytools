0bd517af1182cf4f4e8ff6cf0b7fda73daa40a94
nonnax <ironald@gmail.com>
2022-12-10 14:08:38 +0800

    add MathExt

---
+++ lib/math/math_ext.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# Id$ nonnax 2022-12-09 10:58:57
+require_relative 'numeric_ext'
+
+module MathExt
+ include NumericExt
+ refine Numeric do
+  def delta_change(to)
+   Float(to)/self-1
+  end
+  alias delta delta_change
+ end
+end
