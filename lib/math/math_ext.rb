fae3e6187b0abc874ab1c3c7541f37272afe3f3f
nonnax <ironald@gmail.com>
2022-12-12 15:46:57 +0800

    add MathExt Numeric and Enumerable refinements

--- lib/math/math_ext.rb
+++ lib/math/math_ext.rb
@@ -9,5 +9,31 @@ module MathExt
    Float(to)/self-1
   end
   alias delta delta_change
+  def to_percent
+    self * 100
+  end
  end
+
+ refine Enumerable do
+  def mean
+   self.sum/self.size.to_f
+  end
+
+  def delta_change
+   self.each_cons(2).map{|a, b| a.delta_change(b) }
+  end
+
+  def moving_average(n)
+   self
+   .reverse
+   .each_cons(n).map{|cons| cons.mean }
+   .reverse
+  end
+
+  def to_percent
+   self.map{|x| x * 100}
+  end
+
+ end
+
 end
