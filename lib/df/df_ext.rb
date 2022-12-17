c24dfcf7d4fad72b1062548913c91f45441d53f6
nonnax <ironald@gmail.com>
2022-12-17 12:24:42 +0800

    refactor map_as_strings

--- lib/df/df_ext.rb
+++ lib/df/df_ext.rb
@@ -3,12 +3,18 @@
 
 # Id$ nonnax 2022-12-05 15:35:08
 
-module ArrayExt
-  refine Array do
+module ObjectExt
+  refine Object do
     def deep_dup
       Marshal.load(Marshal.dump(self))
     end
+  end
+end
 
+module ArrayExt
+  using ObjectExt
+
+  refine Array do
     def ljust(width=1, padding = nil)
       dup + ([padding] * (width - size))
     end
@@ -17,9 +23,9 @@ module ArrayExt
       map(&:size).max
     end
 
-    def ljust_rows(width = nil)
+    def ljust_rows(width = nil, padding = nil)
       max_width = width || longest_row
-      map { |r| r.ljust(max_width) }
+      map { |r| r.ljust(max_width, padding) }
     end
 
     def to_hash
@@ -37,7 +43,8 @@ module ArrayExt
       deep_dup
         .map
         .with_index do |r, i|
-          block ? block.call(r.map(&:to_s), i) : r
+          r_string = r.map(&:to_s)
+          block ? block.call(r_string, i) : r_string
         end
     end
 
@@ -65,10 +72,10 @@ module HashExt
       map(&:flatten)
     end
 
-    def ljust_values(width=nil)
+    def ljust_values(width=nil, padding=nil)
       max_width = width || values.longest_row
       dup
-        .transform_values{|v| v.ljust(max_width)}
+        .transform_values{|v| v.ljust(max_width, padding)}
     end
 
     def transpose
@@ -80,7 +87,7 @@ module HashExt
 
     def to_table
       to_flat_array
-      .to_table
+        .to_table
     end
   end
 end
